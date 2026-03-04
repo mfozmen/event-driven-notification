34aebdbf792846b145174c86745148fc7b30e87d
Mehmet Fahri Özmen <mehmetfahriozmen@gmail.com>
2026-03-04 17:15:39 +0300

    feat: added cancel notifcations controller.

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/notifications', [NotificationController::class, 'index']);
 Route::post('/notifications', [NotificationController::class, 'store']);
 Route::get('/notifications/{notification}', [NotificationController::class, 'show']);
+Route::patch('/notifications/{notification}/cancel', [NotificationController::class, 'cancel']);
